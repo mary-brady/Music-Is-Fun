d65df4adedd40585159288f0c9f172875400247f
Mary Brady <emceebrady@gmail.com>
2018-08-12 14:04:17 -0600

    moar css changes

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -10,14 +10,15 @@ function drawSongs(results) {
   //YOUR CODING STARTS HERE
   let template = ''
 
+
   for (let i = 0; i < results.length; i++) {
     const song = results[i];
     template += `
-    <div class="col-sm-3 text-center">
+    <div class="col-xs-3 text-center justify-content-center">
     <div class="card" style="width 18rem;">
       <img class="card-img-top" src="${song.albumArt}" alt="album art"</p>
       <div class="card body">
-      <audio control> <source src="${song.preview}" type="audio/mpeg">
+      <audio controls> <source src="${song.preview}" type="audio/mpeg">
       </audio>
       <h5><b>${song.title}</b></h5>
       <h6 class="text-muted">${song.artist}</h6>
@@ -30,6 +31,26 @@ function drawSongs(results) {
   }
   document.getElementById('songs').innerHTML = template
 }
+//   for (let i = 0; i < results.length; i++) {
+//     const song = results[i];
+//     template += `
+//     <div class="col-sm-3 text-center">
+//     <div class="card" style="width: 18rem;">
+//       <img class="card-img-top" src="${song.albumArt}" alt="album art"</p>
+//       <div class="card body">
+//       <audio control> <source src="${song.preview}" type="audio/mpeg">
+//       </audio>
+//       <h5><b>${song.title}</b></h5>
+//       <h6 class="text-muted">${song.artist}</h6>
+//       <p class="card-text">${song.collection}</p>
+//       <p class="card-text">$${song.price}</p>
+//       </div>
+//     </div>
+//     </div>
+//     `
+//   }
+//   document.getElementById('songs').innerHTML = template
+// }
 
 
 
